4085ef7c2850d07a0bad74fc9ea1a7a5198bad6c
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-09-11 13:40:05 -0400

    Part #1 - BombLine start
    Began work on BombLine script

--- Assets/Scripts/Components/BombLine.cs
+++ Assets/Scripts/Components/BombLine.cs
@@ -14,6 +14,14 @@ public class BombLine : MonoBehaviour
     /// <returns>An array containing all the bomb objects</returns>
     public GameObject[] SpawnBombs()
     {
+        GameObject[] bombSpawns = new GameObject[BombCount];
+
+        for(int i = 0; i < bombSpawns.Length; i++)
+        {
+            bombSpawns[i] = Instantiate(BombPrefab);
+        }
+
+
         return null;
     }
     
