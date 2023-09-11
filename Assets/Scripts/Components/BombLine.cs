aadcc498d8848e8ff7ef8f708595aaeb9a571de8
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-09-11 15:28:46 -0400

    Part#1 - BombLine Complete
    Finished the BombLine Script

--- Assets/Scripts/Components/BombLine.cs
+++ Assets/Scripts/Components/BombLine.cs
@@ -14,15 +14,19 @@ public class BombLine : MonoBehaviour
     /// <returns>An array containing all the bomb objects</returns>
     public GameObject[] SpawnBombs()
     {
-        GameObject[] bombSpawns = new GameObject[BombCount];
+        GameObject[] bombSpawns = new GameObject[BombCount]; //Declares an array of GameObjects with length == BombCount
 
-        for(int i = 0; i < bombSpawns.Length; i++)
+        for(int i = 0; i < bombSpawns.Length; i++) //Runs a for loop that runs the same number of times as the length of the bombSpawns array
         {
-            bombSpawns[i] = Instantiate(BombPrefab);
+            bombSpawns[i] = Instantiate(BombPrefab);  //Instantiates a BombPrefab at the array index i (0 - length of bombSpawns array)
+            bombSpawns[i].transform.position = transform.position + (-transform.up * (BombSpacing * (i + 1))); //Sets the transfrom.position of the GameObject in index i to the
+                                                                                                              //transform.position that this script is attatched to, added to the reverse facing
+                                                                                                              //direction of the object to give it direction, then mulitplied by the BombSpacing float
+                                                                                                              // * the array length + 1 to increase the spacing and distance of each subsequent bomb
         }
 
 
-        return null;
+        return bombSpawns;
     }
     
 }
