039d7537fcdc66237ad6ca32004109ea5695ca99
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-09-17 18:02:57 -0400

    Part #2 - PowerUps comments
    Added comments to the method

--- Assets/Scripts/Components/PowerUps.cs
+++ Assets/Scripts/Components/PowerUps.cs
@@ -17,21 +17,24 @@ public class PowerUps : MonoBehaviour
     /// <returns>An array of the spawned power ups, in counter clockwise order.</returns>
     public GameObject[] SpawnPowerUps()
     {
-        shipPosition = transform.position;
-        placementAngle = circumference / PowerUpCount;
+        shipPosition = transform.position; //Sets the shipPosition to the position of the object this script is attatched to
+        placementAngle = circumference / PowerUpCount; //Sets the placement angle = to the valuue of the circumference divided by the PowerUpCount to keep spacing consistent if PowerUpCount is changed
 
-        GameObject[] powerUpOrbit = new GameObject[PowerUpCount];
+        GameObject[] powerUpOrbit = new GameObject[PowerUpCount]; //Creates a GameObject array with length equal to the PowerUpCount
 
         for (int i = 0; i < powerUpOrbit.Length; i++)
         {
+            powerUpOrbit[i] = Instantiate(PowerUpPrefab); //Fills each index in the array with a new PowerUpPrefab
 
-            powerUpSpawnPoint = new Vector3 (shipPosition.x + Mathf.Cos(placementAngle * i * Mathf.Deg2Rad)  * PowerUpRadius, 
-                shipPosition.y + Mathf.Sin(placementAngle * i * Mathf.Deg2Rad) * PowerUpRadius, 0);
+            //Sets the powerUpSpawnPoint using the point on a circle formula
+            //Multiplying the placementAngle by i acts as a modifier to consistently change the placement of powerups around the player
+            powerUpSpawnPoint = new Vector3(shipPosition.x + Mathf.Cos(placementAngle * i * Mathf.Deg2Rad) * PowerUpRadius,
+                shipPosition.y + Mathf.Sin(placementAngle * i * Mathf.Deg2Rad) * PowerUpRadius, 0); 
 
-            powerUpOrbit[i] = Instantiate(PowerUpPrefab, powerUpSpawnPoint, Quaternion.identity);
+            powerUpOrbit[i].transform.position = powerUpSpawnPoint; //Sets the position of each index in the powerUpOrbit array equal to the current powerUpSpawnPoint
 
         }
 
-        return powerUpOrbit;
+        return powerUpOrbit; //Returns the powerUpOrbit array
     }
 }
