691b399ba4ac4f877c3f183f1adfd28eb7b1f552
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-09-17 17:12:39 -0400

    Part #2 - SpawnPowerUps progress
    Started the SpawnPowerUps method - incomplete

--- Assets/Scripts/Components/PowerUps.cs
+++ Assets/Scripts/Components/PowerUps.cs
@@ -6,6 +6,8 @@ public class PowerUps : MonoBehaviour
     public GameObject PowerUpPrefab;
     public int PowerUpCount = 3;
     public float PowerUpRadius = 1;
+    
+
 
     /// <summary>
     /// Spawn a circle of PowerUpCount power up prefabs stored in PowerUpPrefab, evenly spaced, around the player with a radius of PowerUpRadius
@@ -13,6 +15,17 @@ public class PowerUps : MonoBehaviour
     /// <returns>An array of the spawned power ups, in counter clockwise order.</returns>
     public GameObject[] SpawnPowerUps()
     {
+        Vector3 shipPosition = transform.position;
+
+
+        GameObject[] PowerUpOrbit = new GameObject[PowerUpCount];
+
+        for (int i = 0; i < PowerUpOrbit.Length; i++)
+        {
+            PowerUpOrbit[i] = Instantiate(PowerUpPrefab);
+
+        }
+
         return null;
     }
 }
