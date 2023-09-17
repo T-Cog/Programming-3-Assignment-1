6c29ae9e9056ba9f6d84e897b66d7ed93ee874cc
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-09-17 17:46:16 -0400

    Part #2 - PowerUps Complete
    Finished the PowerUps Method for Part 2

--- Assets/Scripts/Components/PowerUps.cs
+++ Assets/Scripts/Components/PowerUps.cs
@@ -6,8 +6,10 @@ public class PowerUps : MonoBehaviour
     public GameObject PowerUpPrefab;
     public int PowerUpCount = 3;
     public float PowerUpRadius = 1;
-    
-
+    private float placementAngle;
+    private float circumference = 360f;
+    private Vector3 shipPosition;
+    private Vector3 powerUpSpawnPoint;
 
     /// <summary>
     /// Spawn a circle of PowerUpCount power up prefabs stored in PowerUpPrefab, evenly spaced, around the player with a radius of PowerUpRadius
@@ -15,17 +17,21 @@ public class PowerUps : MonoBehaviour
     /// <returns>An array of the spawned power ups, in counter clockwise order.</returns>
     public GameObject[] SpawnPowerUps()
     {
-        Vector3 shipPosition = transform.position;
+        shipPosition = transform.position;
+        placementAngle = circumference / PowerUpCount;
 
+        GameObject[] powerUpOrbit = new GameObject[PowerUpCount];
 
-        GameObject[] PowerUpOrbit = new GameObject[PowerUpCount];
-
-        for (int i = 0; i < PowerUpOrbit.Length; i++)
+        for (int i = 0; i < powerUpOrbit.Length; i++)
         {
-            PowerUpOrbit[i] = Instantiate(PowerUpPrefab);
+
+            powerUpSpawnPoint = new Vector3 (shipPosition.x + Mathf.Cos(placementAngle * i * Mathf.Deg2Rad)  * PowerUpRadius, 
+                shipPosition.y + Mathf.Sin(placementAngle * i * Mathf.Deg2Rad) * PowerUpRadius, 0);
+
+            powerUpOrbit[i] = Instantiate(PowerUpPrefab, powerUpSpawnPoint, Quaternion.identity);
 
         }
 
-        return null;
+        return powerUpOrbit;
     }
 }
