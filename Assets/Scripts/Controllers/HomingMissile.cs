30149a76c187c4288edf39ac5d6605eab393a7ea
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-09-30 15:11:11 -0400

    Part 3 - Method 2 Complete
    Finished part 3

--- Assets/Scripts/Controllers/HomingMissile.cs
+++ Assets/Scripts/Controllers/HomingMissile.cs
@@ -9,6 +9,16 @@ public class HomingMissile : MonoBehaviour
     // In Update, you should rotate and move the missile to rotate it towards the player.  It should move forward with ForwardSpeed and rotate at RotateSpeedInDeg.
     void Update()
     {
+        Vector3 enemyPos = GameController.GetEnemyObject().transform.position;
+        Vector3 enemyPosLocal = transform.InverseTransformPoint(enemyPos);
 
+        float angle = Mathf.Atan2(enemyPosLocal.x, enemyPosLocal.y) * Mathf.Rad2Deg;
+        float clampedAngle = Mathf.Clamp(angle, -RotateSpeedInDeg * Time.deltaTime, RotateSpeedInDeg * Time.deltaTime);
+
+        Vector3 velocity = new Vector3(0, ForwardSpeed * Time.deltaTime, 0);
+        Vector3 angularVelocity = new Vector3(0, 0, -clampedAngle);
+
+        transform.Translate(velocity);
+        transform.Rotate(angularVelocity);
     }
 }
