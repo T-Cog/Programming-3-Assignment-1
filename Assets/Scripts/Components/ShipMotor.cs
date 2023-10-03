c4fbe35215fd9865a0e3bfb9fbbb079fdbe75d85
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-10-03 18:10:36 -0400

    Update ShipMotor.cs
    Added Comments

--- Assets/Scripts/Components/ShipMotor.cs
+++ Assets/Scripts/Components/ShipMotor.cs
@@ -7,8 +7,6 @@ public class ShipMotor : MonoBehaviour
     public float AccelerationTime = 1;
     public float DecelerationTime = 1;
     public float MaxSpeed = 5;
-
-
     Vector2 velocity = Vector2.zero;
 
     /// <summary>
@@ -17,22 +15,24 @@ public class ShipMotor : MonoBehaviour
     /// <param name="input">The input from the player. The possible range of values for x and y are from -1 to 1.</param>
     public void HandleMovementInput( Vector2 input )
     {
-        Camera cam = GameController.GetCamera();
-        Vector2 relativeCamInput = cam.transform.TransformPoint(input);
+        Camera cam = GameController.GetCamera(); //Calls the GetCamera Method and stores it
+        Vector2 relativeCamInput = cam.transform.TransformPoint(input); //Changes the input to be relative to the camera in local space
         
-        Vector2 acceleration = relativeCamInput * (MaxSpeed / AccelerationTime);
+        Vector2 acceleration = relativeCamInput * (MaxSpeed / AccelerationTime); //Sets acceleration relative to camera input using rise over run
 
-        if (input == Vector2.zero)
+        //if statement that begins deceleration if there is no inpu
+        if (input == Vector2.zero) 
         {
-            Vector2 normalizedVel = Normalize(velocity);
-            acceleration = -normalizedVel * (MaxSpeed / DecelerationTime);
+            Vector2 normalizedVel = Normalize(velocity); //Normalizes and stores velocity
+            acceleration = -normalizedVel * (MaxSpeed / DecelerationTime); //Sets acceleration relative to normalized velocity using rise over run to keep acceleration consistent
         }
-        velocity += acceleration * Time.deltaTime;
-        velocity = Vector2.ClampMagnitude(velocity, MaxSpeed);
-        transform.position += (Vector3)velocity * Time.deltaTime;
+        velocity += acceleration * Time.deltaTime; //Adds acceleration value to velocity in deltaTime
+        velocity = Vector2.ClampMagnitude(velocity, MaxSpeed); //Clamps the Vector to cap at MaxSpeed
+        transform.position += (Vector3)velocity * Time.deltaTime; //Adds the velocity to the velocity vector in deltaTime
     }
 
 
+    //Method to normalize vectors
     private Vector2 Normalize(Vector2 v)
     {
         float magnitude = Magnitude(v);
@@ -45,6 +45,7 @@ public class ShipMotor : MonoBehaviour
         return v;
     }
 
+    //Method to find the magnitutde of vectors
     private float Magnitude(Vector2 v)
     {
 
