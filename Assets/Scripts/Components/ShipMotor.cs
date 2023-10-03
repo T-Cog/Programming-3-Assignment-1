4657dcc24a4a22c65d09dfc76822fa6d3c92717d
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-10-03 12:59:16 -0400

    Part 4 - Method 1 WIP
    Started REAL progress on Method 1

--- Assets/Scripts/Components/ShipMotor.cs
+++ Assets/Scripts/Components/ShipMotor.cs
@@ -6,7 +6,8 @@ public class ShipMotor : MonoBehaviour
 {
     public float AccelerationTime = 1;
     public float DecelerationTime = 1;
-    public float MaxSpeed = 1;
+    public float MaxSpeed = 5;
+    private float currentSpeed;
     bool moving = false;
     /// <summary>
     /// Move the ship using it's transform only based on the current input vector.
@@ -14,23 +15,49 @@ public class ShipMotor : MonoBehaviour
     /// <param name="input">The input from the player. The possible range of values for x and y are from -1 to 1.</param>
     public void HandleMovementInput( Vector2 input )
     {
-        
-        
-        Vector3 verticalVelocity = new Vector3(0, MaxSpeed * (AccelerationTime * Time.deltaTime), 0);
-        // Vector2 horizontalVelocity = new Vector2(MaxSpeed / (AccelerationTime * Time.deltaTime), 0);
 
-        
+        Vector3 verticalVelocity = new Vector3(0, currentSpeed * Time.deltaTime, 0);
+        Vector3 horizontalVelocity = new Vector3(currentSpeed * Time.deltaTime, 0, 0);
 
-        if (Input.GetKeyDown("up"))
-            {
+
+        if (input.y > 0.1)
+        {
             moving = true;
-           
-            transform.position += new Vector3(0, 5 * Time.deltaTime, 0);
-            }
+
+            currentSpeed += AccelerationTime * Time.deltaTime;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, MaxSpeed);
+
+            transform.Translate(verticalVelocity);
+            Debug.Log("Goin UP");
+        }
+        else if (input.y < -0.1)
+        {
+            moving = true;
+            currentSpeed += AccelerationTime * Time.deltaTime;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, MaxSpeed);
+
+            transform.Translate(-verticalVelocity);
+            Debug.Log("Goin DOWN");
+        }
+        else if (input.x > 0.1)
+        {
+            moving = true;
+            transform.Translate(horizontalVelocity);
+            Debug.Log("Goin RIGHT");
+        }
+        else if (input.x < -0.1)
+        {
+            moving = true;
+            transform.Translate(-horizontalVelocity);
+            Debug.Log("Goin LEFT");
+        }
         else
         {
             moving = false;
-            transform.position = Vector2.zero;
+
+            currentSpeed = 0;
+            transform.Translate(Vector3.zero);
+            Debug.Log("Stop");
         }
     }
 
