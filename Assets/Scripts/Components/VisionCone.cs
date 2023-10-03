112ed4b82983ea779ce54514ac65c2fc920b7e2e
T-Cog <71793105+T-Cog@users.noreply.github.com>
2023-10-03 11:52:40 -0400

    Part 3 - Comments
    Added comments to part 3 methods

--- Assets/Scripts/Components/VisionCone.cs
+++ Assets/Scripts/Components/VisionCone.cs
@@ -16,29 +16,31 @@ public class VisionCone : MonoBehaviour
     /// <returns>Whether the player is within the enemy's vision cone.</returns>
     public bool IsPlayerInVisionCone()
     {
-        Vector3 enemyToPlyr = player.position - transform.position;
-        float distanceToPlayer = DistanceToPlayer();
+        Vector3 enemyToPlyr = player.position - transform.position; //Stores the vector from the enemy to the player
+        float distanceToPlayer = DistanceToPlayer(); //Calls the Distance to player method
 
-        float transformUpMag = Mathf.Sqrt((transform.up.x * transform.up.x) + (transform.up.y * transform.up.y));
+        float transformUpMag = Mathf.Sqrt((transform.up.x * transform.up.x) + (transform.up.y * transform.up.y)); //Caculates the magnitude of the enemy transform.up
 
+        //Calculates the dot product of the vectors of transform.up and distance to the player
         float enemyPlyrDot = ((transform.up.x * enemyToPlyr.x) + (transform.up.y * enemyToPlyr.y) + (transform.up.z * enemyToPlyr.z) 
             / (transformUpMag / distanceToPlayer));
 
-        float angle = Mathf.Acos(enemyPlyrDot / (transformUpMag * distanceToPlayer));
+        float angle = Mathf.Acos(enemyPlyrDot / (transformUpMag * distanceToPlayer)); //Sets an angle between the player and enemy transform.up using the dot product
 
-        bool isPlayerInVisionCone;
+        //If statement used to check if the player is within ViewDistance of the enemy
+        //and if the angle between the enemy and the player is greater than 0 to ensure they are in front of the enemy
+        //and if the angle between the enemy and player is within the enemy's vision cone set by AngleSweepInDegrees
         if ( distanceToPlayer <= ViewDistance && angle * Mathf.Rad2Deg > 0 && angle * Mathf.Rad2Deg <= AngleSweepInDegrees)
         {
-            isPlayerInVisionCone = true;
+            return true; //Returns the method bool as true
         }
         else
         {
-            isPlayerInVisionCone = false;
+            return false; //Returns the method bool as false
         }
-
-        return isPlayerInVisionCone;
     }
     
+    //Method used to calculate the magnitude from the enemy vector to the player vector
     public float DistanceToPlayer()
     {
         Vector3 enemyToPlyr = player.position - transform.position;
--- Assets/Scripts/Controllers/HomingMissile.cs
+++ Assets/Scripts/Controllers/HomingMissile.cs
@@ -9,16 +9,16 @@ public class HomingMissile : MonoBehaviour
     // In Update, you should rotate and move the missile to rotate it towards the player.  It should move forward with ForwardSpeed and rotate at RotateSpeedInDeg.
     void Update()
     {
-        Vector3 enemyPos = GameController.GetEnemyObject().transform.position;
-        Vector3 enemyPosLocal = transform.InverseTransformPoint(enemyPos);
+        Vector3 enemyPos = GameController.GetEnemyObject().transform.position; //Calls the GetEnemyObject method and stores the enemy's position
+        Vector3 enemyPosLocal = transform.InverseTransformPoint(enemyPos); //Sets the enemyPos to local space, relative to the player
 
-        float angle = Mathf.Atan2(enemyPosLocal.x, enemyPosLocal.y) * Mathf.Rad2Deg;
-        float clampedAngle = Mathf.Clamp(angle, -RotateSpeedInDeg * Time.deltaTime, RotateSpeedInDeg * Time.deltaTime);
+        float angle = Mathf.Atan2(enemyPosLocal.x, enemyPosLocal.y) * Mathf.Rad2Deg; //Stores an angle using the enemy's local position
+        float clampedAngle = Mathf.Clamp(angle, -RotateSpeedInDeg * Time.deltaTime, RotateSpeedInDeg * Time.deltaTime); //Clamps the angle min and max to the value of RotateSpeedInDeg, modified by deltaTime 
 
-        Vector3 velocity = new Vector3(0, ForwardSpeed * Time.deltaTime, 0);
-        Vector3 angularVelocity = new Vector3(0, 0, -clampedAngle);
+        Vector3 velocity = new Vector3(0, ForwardSpeed * Time.deltaTime, 0); //Sets a velocity using ForwardSpeed modified by deltaTime
+        Vector3 angularVelocity = new Vector3(0, 0, -clampedAngle); //Sets angularVelocity for rotation using clampedAngle to constrain rotation 
 
-        transform.Translate(velocity);
-        transform.Rotate(angularVelocity);
+        transform.Translate(velocity); //Uses Translate to move the missile using velocity
+        transform.Rotate(angularVelocity); //Uses Rotate to turn the missile using the contstrained rotation
     }
 }
